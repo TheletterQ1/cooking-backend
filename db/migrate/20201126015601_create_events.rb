d44279118dee815999adbc4657010a819ac70dcb
Q <Q@Skylars-MBP.lan1>
2020-11-28 16:16:08 -0800

    second commit

---
+++ db/migrate/20201126015601_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[6.0]
+  def change
+    create_table :events do |t|
+      t.string :title
+      t.string :date
+      t.string :time
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
